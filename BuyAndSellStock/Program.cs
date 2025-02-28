b07ee2d7c687a1892eed77858d0cd8d8661da6d2
Jack Heywood <jackheywood@maritime-mutual.com>
2025-02-28 09:58:32 +0000

    Buy & Sell Stock

---
+++ BuyAndSellStock/Program.cs
@@ -0,0 +1,28 @@
+﻿using BuyAndSellStock;
+
+int[][] testPrices =
+[
+    new[] { 7, 1, 5, 3, 6, 4 },
+    new[] { 1, 2, 3, 4, 5 },
+    new[] { 7, 6, 4, 3, 1 },
+];
+
+var profits1 = testPrices.Select(Solution.MaxProfit1);
+var profits2 = testPrices.Select(Solution.MaxProfit2);
+
+Console.WriteLine("Solution 1:");
+
+foreach (var profit in profits1)
+{
+    Console.WriteLine(profit);
+}
+
+Console.WriteLine("\nSolution 2:");
+
+foreach (var profit in profits2)
+{
+    Console.WriteLine(profit);
+}
+
+Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey();
---
+++ BuyAndSellStock/Solution.cs
@@ -0,0 +1,58 @@
+﻿namespace BuyAndSellStock;
+
+internal class Solution
+{
+    public static int MaxProfit1(int[] prices)
+    {
+        var totalProfit = 0;
+        var profit = 0;
+        var bought = prices[0];
+        var sold = 0;
+
+        foreach (var price in prices)
+        {
+            if (price < bought)
+            {
+                bought = price;
+                sold = price;
+            }
+
+            if (price > sold)
+            {
+                sold = price;
+            }
+
+            var currentProfit = price - bought;
+
+            if (currentProfit > profit)
+            {
+                profit = currentProfit;
+            }
+
+            if (currentProfit < profit)
+            {
+                totalProfit += profit;
+                bought = price;
+                sold = price;
+                profit = 0;
+            }
+        }
+
+        return totalProfit + profit;
+    }
+
+    public static int MaxProfit2(int[] prices)
+    {
+        var profit = 0;
+
+        for (var i = 1; i < prices.Length; i++)
+        {
+            if (prices[i] > prices[i - 1])
+            {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+}
